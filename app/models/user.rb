41e08ba8e4bfeeb15d65ba0284cacb90078d8696
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-14 14:59:04 -0600

    add relationships and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :tips
+
+  validates_presence_of :first_name, 
+                        :last_name, 
+                        :phone,
+                        :email,
+                        :city,
+                        :state,
+                        :password_digest
+
+  validates_uniqueness_of :email
 end
