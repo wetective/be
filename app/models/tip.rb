de0c1b1cb8ea5f2ad6a0b1b5078711acae3f3433
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-14 14:59:13 -0600

    add relationships and validations to Tip model

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,2 +1,9 @@
 class Tip < ApplicationRecord
+  belongs_to :user
+  
+  validates_presence_of :uid,
+                        :location,
+                        :date,
+                        :time,
+                        :description,
 end
