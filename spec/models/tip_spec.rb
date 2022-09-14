403b492522d856f978fbb2cd5553cecfcd5d16f4
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-14 14:58:41 -0600

    test Tip model relationships and validations

--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Tip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:uid) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:time) }
+    it { should validate_presence_of(:description) }
+  end
 end
