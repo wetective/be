c2a6472dd2f78a68fa26b3f21fd3c113186de1bf
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-19 15:43:42 -0400

    Create user controller and serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,31 @@
-class UsersController
+class Api::V1::UsersController < ApplicationController
   
   def index
-    
+    render json: UserSerializer.new(User.all)
   end
   
   def show
-    
+    user = User.find_by_id(params[:id])
+    if user
+      render json: UserSerializer.new(user)
+    else
+      render json: { status: "User Not Found", code: 404, message: "User Not Found" }, status: 404
+    end
+  end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    user = User.new(user_params[:email])
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
+    get '/users/register', to: 'users#new'
+    post '/users/register', to: 'users#create'
     end
   end
 end
