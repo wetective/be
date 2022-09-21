26f8d83709c23bad3971fd4ea16980e4c40c2eab
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-21 10:41:35 -0600

    update README

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show]
 
-    post '/users/register', to: 'users#find_or_create'
-    get '/users/find', to: 'users#show'
+      post '/users/register', to: 'users#find_or_create'
+      get '/users/find', to: 'users#show'
     end
   end
 end
