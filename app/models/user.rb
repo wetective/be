82aec52df43e65c3729f33d6aab51b920b6a1d20
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-21 12:26:17 -0600

    refactor: validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
   has_many :tips
 
-  validates_presence_of :email
+  validates :email, presence: true, uniqueness: true
+
   # validates_presence_of :first_name, 
   #                       :last_name, 
   #                       :phone,
   #                       # :city,
   #                       # :state,
   #                       # :password_digest
-
-  validates_uniqueness_of :email
 
   def self.oauth_user(data)
     user = User.find_or_create_by(email: data[:email]) do |user|
