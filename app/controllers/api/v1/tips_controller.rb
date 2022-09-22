654fcb219f5dfcff3d323b97fdc6ce09f00825cd
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-22 11:30:28 -0600

    Update tips controller

--- app/controllers/api/v1/tips_controller.rb
+++ app/controllers/api/v1/tips_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::TipsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    tips = user.tips
+    render json: TipSerializer.new(tip), status: 201
+  end
 
   def create
     tip = Tip.create(tip_params)
