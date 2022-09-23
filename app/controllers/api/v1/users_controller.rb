56d705dae65c3ff03acbf919d2bfefa4565f826d
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-22 20:40:09 -0400

    Add logic for dealing with incoming api data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,36 +1,41 @@
 class Api::V1::UsersController < ApplicationController
-  
-  def index
-    render json: UserSerializer.new(User.all)
-  end
-  
-  def show
-    user = User.find_by_id(params[:id])
-    if user
-      render json: UserSerializer.new(user)
+
+  def find_or_create
+    user = User.find_or_create_user(user_params)
+    if user && user_params[:oauth]
+      if user.save
+        render json: UserSerializer.new(user), status: 201
+      else
+        render json: { status: "User Not Found", code: 404, message: "User Not Found" }, status: 404
+      end
+    elsif user
+      if user.save
+        render json: UserSerializer.new(user), status: 201
+      elsif user.authenticate(user_params[:password])
+        render json: UserSerializer.new(user), status: 200
+      else
+        render json: { status: "Incorrect Password", code: 404, message: "Incorrect Password" }, status: 404
+      end
     else
       render json: { status: "User Not Found", code: 404, message: "User Not Found" }, status: 404
     end
   end
 
-  def new
-  end
-
-  def find_or_create
-    user = User.oauth_user(user_params)
-    render json: UserSerializer.new(user)
-    # user = User.new(email: user_params[:email])
-    # if user.save
-    #   render json: UserSerializer.new(user), status: :created
-    # else
-    #   user = User.find_by_id(user_params[:email])
-    #   binding.pry
-    #   render json: UserSerializer.new(user), status: 200
-    # end
+  def find
+    user = User.find_user(user_params)
+    if user
+      if user.authenticate(user_params[:password])
+        render json: UserSerializer.new(user), status: 200
+      else
+        render json: { status: "Incorrect Password", code: 404, message: "Incorrect Password" }, status: 404
+      end
+    else
+      render json: { status: "User Not Found", code: 404, message: "User Not Found" }, status: 404
+    end
   end
 
   private
   def user_params
-    params.permit(:email, :name)
+    params.permit(:email, :password, :oauth)
   end
 end
